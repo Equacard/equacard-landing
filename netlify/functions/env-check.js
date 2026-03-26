d6994155abfe4fde8f9cf92f938bd3206f042d3a
Riccardo <riccaangeletti@icloud.com>
2026-03-26 11:15:41 +0100

    Add env-check function

---
+++ netlify/functions/env-check.js
@@ -0,0 +1,9 @@
+﻿exports.handler = async () => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      SUPABASE_URL: process.env.SUPABASE_URL ? "OK" : "MISSING",
+      SUPABASE_KEY: process.env.SUPABASE_KEY ? "OK" : "MISSING"
+    })
+  };
+};
